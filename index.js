7b88f47cf8ef430eff3b0fe2e282fee54ad6de30
mrkrca <aleksa.djuric.dj@gmail.com>
2025-01-07 21:50:01 +0100

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const db = new pg.Client({
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT, 10),
 });
 
 db.connect();
